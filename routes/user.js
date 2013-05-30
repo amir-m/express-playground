1a12dd766e88e1347bce8470f0f5342ca18c1b93
Amir <amir.moravej@gmail.com>
2013-05-30 11:58:05 -0400

    POST /login handler.

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,11 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+	res.send("respond with a resource");
+};
+
+exports.login = function(req, res){
+	console.log('email: %s, \tpassword: %s', req.param('email'), req.param('password'));
+	res.status(200);
+	res.send('Succeeded :)');
 };
