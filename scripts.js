4d0cf2523ea9fd170baab8d3e0d4eb905df16804
Amir <amir.moravej@gmail.com>
2013-05-30 19:46:24 -0400

    minor modifications

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 var http = require('http');
 
+loginUser();
+
+// registerUser();
+
 function registerUser(){
 	var options = {
 		hostname: 'localhost',
@@ -30,6 +34,40 @@ function registerUser(){
 	};
 
 	console.log(JSON.stringify(user));
+	// write data to request body JSON.stringify(user)
+	req.write(JSON.stringify(user));
+
+	req.end();
+};
+
+function loginUser(){
+	var options = {
+		hostname: 'localhost',
+		port: 8080,
+		path: '/login',
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json"
+		}
+	};
+
+	var req = http.request(options, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log('BODY: ' + chunk);
+		});
+	});
+
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	var user = {
+		'email': 'amir@doob.io',
+		'password': 'mypassword'
+	};
+
 	// write data to request body JSON.stringify(user)
 	req.write(JSON.stringify(user));
 
