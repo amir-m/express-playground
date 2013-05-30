7b3125b584d0e0726bc95689429e75af76bd19ae
Amir <amir.moravej@gmail.com>
2013-05-30 13:38:45 -0400

    define UserSchema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,29 @@
 exports.User = function(mongoose) {
 
+	var UserSchema = mongoose.Scchema({
+		id: mongoose.Schema.Types.ObjectId,
+		email: {type: String, required: true},
+		password: {type: String, required: true},
+		name: {type: String, required: true},
+		isMale: Boolean,
+		dob: Date,
+		createdAt: Date,
+		lastModifiedAt: Date,
+		decativated: {
+			timestamp: Date
+		},
+		tags: [String],
+		description: String
+	});
+
+	// schema settings
+	UserSchema.set('autoIndex', false);
+	UserSchema.index({
+		name: 1,
+		email: 1,
+		tags: 1
+	});
+
 	var create = function(){};
 
 	var update = function(){};
